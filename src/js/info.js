9f6f96a2aedb6715ca8ef2a0d3a701307e4fee1b
mid0111 <m.tajima0111@gmail.com>
2012-06-23 19:13:47 +0900

    level, score, line

--- src/js/info.js
+++ src/js/info.js
@@ -1,16 +1,48 @@
 (function($) {
-	function changeColor(target, color) {
-		$(target).css("background", color);
+
+	var lineCnt;
+	var score;
+	var level;
+
+	// ラインをインクリメントする
+	var lineClear = function(event, num) {
+		lineCnt = eval(lineCnt + num);
+		$("#lines .info-value").text(lineCnt);
+		calcScoreAndLevel();
+	};
+	
+	// レベル、スコア、ラインの値を出力する
+	function write() {
+		$("#lines .info-value").text(lineCnt);
+		$("#score .info-value").text(score);
+		$("#level .info-value").text(level);
+	}
+
+	// スコアとレベルを計算する
+	function calcScoreAndLevel() {
+		score = Math.round(Math.sqrt(lineCnt)) * 10;
+		$("#score .info-value").text(score);
+		calcLevel();
+	}
+
+	// レベルを計算する
+	function calcLevel() {
+		level = (Math.round(score / 17));
+		if (level === 0) {
+			level = 1;
+		}
+		$("#level .info-value").text(level);
 	}
 
 	// onready
 	$(function() {
-		changeColor($("#info"), "blue");
-		
-		var level = $("#level .info-value");
-		
-		$("body").on("levelchange", function() {
-			level.text(10);
-		});
+		lineCnt = 0;
+		score = 0;
+		level = 1;
+		write();
+
+		// event listener
+		$("body").on("lineClear", lineClear);
 	});
+
 })(jQuery);
